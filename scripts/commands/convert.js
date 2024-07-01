2aa826d2608230ce45746a222687a6fa67931029
𝐃𝐀𝐕𝐈𝐃 𝐑𝐀𝐉 <davidraj404247@gmail.com>
2024-07-01 14:00:30 +0600

    Update convert.js

--- scripts/commands/convert.js
+++ scripts/commands/convert.js
@@ -19,7 +19,7 @@ var audioss = []
     var { data } = await axios.get(audio ,{  method: 'GET',  responseType: 'arraybuffer' });
                 fs.writeFileSync(__dirname + "/cache/vdtoau.m4a", Buffer.from(data, 'utf-8'));
   audioss.push(fs.createReadStream(__dirname + "/cache/vdtoau.m4a"));
-    var msg = { body : "𝗖𝗼𝘃𝗲𝗿𝘁 𝘀𝗮𝗻𝗴 𝗠𝗣𝟯  ️🎶\n𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐧𝐚𝐲𝐚𝐧", attachment: audioss}
+    var msg = { body : "𝗖𝗼𝘃𝗲𝗿𝘁 𝘀𝗮𝗻𝗴 𝗠𝗣𝟯  ️🎶\n𝗗𝗔𝗩𝗜𝗗 𝗥𝗔𝗝", attachment: audioss}
   api.sendMessage(msg, event.threadID, event.messageID)
 } catch(e){
     console.log(e)
